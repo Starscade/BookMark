2fab34a16f9c1ca63bd9d53f4fdbf3923e662e7c
Starscade <angus@starsca.de>
2026-02-21 11:27:40 -0800

    Added tests.

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+md_input=examples/input.md
+html_target=examples/output.html
+tmp_output=/tmp/bookmark_test.html
+
+bookmark $md_input > $tmp_output
+echo >> $tmp_output
+
+if diff -q $tmp_output $html_target > /dev/null; then
+	printf '\n  \033[1;32m✓\033[0m PASS\n\n'
+else
+	printf '\n  \033[1;31m✗\033[0m FAIL\n\n'
+fi
+
+rm $tmp_output
